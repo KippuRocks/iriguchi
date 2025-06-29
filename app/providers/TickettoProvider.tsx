60d365cdf17ef55e169e0d9f80b1acc32a2f85c8
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2025-06-29 12:15:21 -0500

    chore(app/providers): connect to blockchain ❤️

--- app/providers/TickettoProvider.tsx
+++ app/providers/TickettoProvider.tsx
@@ -3,9 +3,11 @@
 import { useEffect, useState } from "react";
 
 import { TickettoClientBuilder } from "@ticketto/protocol";
-import { TickettoWebStubConsumer } from "@ticketto/web-stub";
 import { type AccountId } from "@ticketto/types";
 import { TickettoClientProvider } from "./TickettoClientProvider";
+import { KippuConfig, KippuPAPIConsumer } from "@kippurocks/libticketto-papi";
+import { createClient } from "polkadot-api";
+import { getWsProvider } from "polkadot-api/ws-provider/web";
 
 export function TickettoProvider({
   accountId,
@@ -16,13 +18,27 @@ export function TickettoProvider({
 }) {
   const [isClientSide, setIsClientSide] = useState(false);
   const builder = new TickettoClientBuilder()
-    .withConsumer(TickettoWebStubConsumer)
+    .withConsumer(KippuPAPIConsumer)
     .withConfig({
+      consumerSettings: {
+        apiEndpoint: process.env.NEXT_PUBLIC_KIPPU_API,
+        client: createClient(
+          getWsProvider(
+            process.env.NEXT_PUBLIC_CHAIN_ENDPOINT ?? "wss://kreivo.kippu.rocks"
+          )
+        ),
+        eventsContractAddress: process.env.NEXT_PUBLIC_EVENTS_CONTRACT_ADDRESS,
+        ticketsContractAddress:
+          process.env.NEXT_PUBLIC_TICKETS_CONTRACT_ADDRESS,
+        merchantId: process.env.NEXT_PUBLIC_MERCHANT_ID,
+      },
       accountProvider: {
         getAccountId: () => "5DD8bv4RnTDuJt47SAjpWMT78N7gfBQNF2YiZpVUgbXkizMG",
-        sign: (payload: Uint8Array) => Promise.resolve(payload),
+        sign() {
+          throw new Error("NotImplemented");
+        },
       },
-    });
+    } as KippuConfig);
 
   useEffect(() => {
     setIsClientSide(true);
